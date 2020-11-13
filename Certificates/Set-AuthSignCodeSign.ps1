42205c0b46202c98179871b7e162fe411351f299
Ricardo Gomes <ricardo.gomes@navinet.work>
2020-11-13 17:29:20 +0000

    Certificate powershell scripts added

---
+++ Certificates/Set-AuthSignCodeSign.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+	[ValidateSet("SHA1","SHA265","SHA512")]
+	 [String]$HashAlgorithm = "SHA512",
+	[ValidateScript({ Test-Path $_ })]
+	 [String]$File = $null
+)
+Try{
+	#$TimestampServer = "http://timestamp.comodoca.com/authenticode"
+    $TimestampServer = "http://timestamp.verisign.com/scripts/timstamp.dll"
+	$Certificate = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert
+	if($Certificate){
+		Set-AuthenticodeSignature -Certificate $Certificate -IncludeChain all -TimestampServer $TimestampServer -HashAlgorithm $HashAlgorithm -FilePath $File
+	}
+}Catch{
+	$_.Exception.Message
+    Break
+}
