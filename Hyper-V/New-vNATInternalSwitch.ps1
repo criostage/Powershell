59fbbd0f83ed35006660153a477566f367bc2212
Ricardo Gomes <ricardo.gomes@navinet.work>
2021-05-03 14:28:54 +0100

    new Hyper-V and Surface Hub scripts

---
+++ Hyper-V/New-vNATInternalSwitch.ps1
@@ -0,0 +1,3 @@
+﻿New-VMSwitch -SwitchName "vNATInternalSwitch" -SwitchType Internal
+New-NetIPAddress -IPAddress 192.168.2.1 -PrefixLength 24 -InterfaceIndex (Get-NetIPInterface | Where-Object { ($_.InterfaceAlias -match "vNATInternalSwitch") -and ($_.AddressFamily -eq "IPv4") }).ifIndex
+New-NetNat -Name "vNat" -InternalIPInterfaceAddressPrefix 192.168.2.0/24
---
+++ Surface/New-SurfaceHubDevice Account.ps1
@@ -0,0 +1,23 @@
+Connect-AzureAD
+Connect-ExchangeOnline -ShowProgress $true
+
+$userUPN="DeviceUPN@domain.com"
+$alias="SurfaceHub"
+$displayName="Country - City - Room 10"
+$password="AwsomePasword"
+$Location="US"
+# Run 'Get-AzureADSubscribedSku | Select SkuPartNumber'
+$SubscriptionName="SPE_E3"
+
+#Install-Module -Name ExchangeOnlineManagement
+New-Mailbox -MicrosoftOnlineServicesID $userUPN -Alias $alias -Name $displayName -Room -EnableRoomMailboxAccount $true -RoomMailboxPassword (ConvertTo-SecureString -String $password -AsPlainText -Force)
+    
+# Get the license Sku ID
+$License = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
+$License.SkuId = (Get-AzureADSubscribedSku | Where-Object -Property SkuPartNumber -Value $SubscriptionName -EQ).SkuID
+$LicensesToAssign = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+$LicensesToAssign.AddLicenses = $License
+    
+# Set Location and apply License
+Set-AzureADUser -ObjectId $userUPN -UsageLocation $Location
+Set-AzureADUserLicense -ObjectId $userUPN -AssignedLicenses $LicensesToAssign
